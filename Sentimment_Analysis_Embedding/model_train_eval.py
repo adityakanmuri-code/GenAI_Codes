e1ebf6bf4ec73551ec42c1d108f752f5fcd73ca8
Aditya <adityakanmuri@gmail.com>
2026-04-18 19:44:21 +0530

    Added Folder Structure for Embedding Models

---
+++ Sentimment_Analysis_Embedding/model_train_eval.py
@@ -0,0 +1,86 @@
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, f1_score,classification_report,confusion_matrix
+from sklearn.model_selection import train_test_split,cross_val_score
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import GridSearchCV
+
+
+class model_trainer():
+
+    def __split_data(self, X, y):
+        X_train,X_temp,y_train,y_temp = train_test_split(X,y,test_size=0.4,stratify=y)
+        X_val,X_test,y_val,y_test = train_test_split(X_temp,y_temp,test_size=0.2,stratify=y_temp)
+        return X_train,X_val,X_test,y_train,y_val,y_test
+
+    def __reduce_imbalances(self, X_train, y_train):
+        ros = RandomOverSampler(random_state=42)
+        return ros.fit_resample(X_train, y_train)
+
+    def __encode_text_BOW(self, X, y):
+        X_train,X_val,X_test,y_train,y_val,y_test = self.__split_data(X, y)
+
+        bow = CountVectorizer(max_features=2500, ngram_range=(1, 3))
+        X_train_bow = bow.fit_transform(X_train)
+        X_test_bow = bow.transform(X_test)
+        X_val_bow = bow.transform(X_val)
+
+        X_train_resampled, y_train_resampled = self.__reduce_imbalances(X_train_bow, y_train)
+
+        return X_train_resampled, X_test_bow,y_train_resampled,y_test, bow
+
+    def __encode_text_TFIDF(self, X, y):
+        X_train,X_val,X_test,y_train,y_val,y_test = self.__split_data(X, y)
+
+        tfidf = TfidfVectorizer(max_features=15000, ngram_range=(1, 3))
+        X_train_tfidf = tfidf.fit_transform(X_train)
+        X_test_tfidf = tfidf.transform(X_test)
+        X_val_tfidf = tfidf.transform(X_val)
+
+        X_train_resampled, y_train_resampled = self.__reduce_imbalances(X_train_tfidf, y_train)
+
+        return X_train_resampled, X_test_tfidf, y_train_resampled, y_test, tfidf
+
+    def __calculate_cv_score(self,model,X,y):
+        return cross_val_score(model,X,y,cv=5,scoring='f1_weighted').mean()
+
+    def train_model(self, X, y, encode_tech: str = 'TFIDF'):
+        if encode_tech == 'BOW':
+            X_train,X_test,y_train,y_test,vectorizer = self.__encode_text_BOW(X, y)
+        elif encode_tech == 'TFIDF':
+            X_train,X_test,y_train,y_test,vectorizer = self.__encode_text_TFIDF(X, y)
+        else:
+            raise ValueError("Invalid encoding type. Use 'BOW' or 'TFIDF'")
+
+        models = {
+            'Naive_Bayes' : MultinomialNB(),
+            'Logistic_Regression' : LogisticRegression(),
+            'Linear_SVC' : LinearSVC()
+        }
+        best_score = -1
+        best_model = None
+
+        for name,model in models.items():
+            model.fit(X_train,y_train)
+            scores = self.__model_eval(model,X_test,y_test)
+            cv_score = self.__calculate_cv_score(model,X_test,y_test)
+            #Fetching the best model 
+            if cv_score >= best_score:
+                if scores['F1_Score'] > best_score:
+                    best_model = model
+                    best_model_name = model.__class__.__name__
+                    best_score = scores['F1_Score']
+        return best_model,best_model_name,best_score,vectorizer
+    
+    def __model_eval(self, model, X_test, y_test):
+        y_pred = model.predict(X_test)
+
+        return {
+            'Model_Name': model.__class__.__name__,
+            'Accuracy': accuracy_score(y_test, y_pred),
+            'F1_Score': f1_score(y_test, y_pred,average='weighted'),
+            'Confusion Matrix' : confusion_matrix(y_test,y_pred),
+            'Classification Report' : classification_report(y_test,y_pred)
+        }
---
+++ Sentimment_Analysis_Embedding/text_preprocessing.py
@@ -0,0 +1,118 @@
+'''
+Text Preprocessing steps involves the following steps :
+Step 1 : Text Ingestion of kaggle dataset for Amazon Reviews
+Step 2 : Data Cleaning and building vocabulary for using in BOW and TFIDF
+'''
+import pandas as pd 
+import re
+import numpy as np
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+import sys
+from tqdm import tqdm
+import emoji
+
+
+#Step 1 : Data Ingestion
+class data_ingestion():
+    def ingest_data(self,file_name:str = '',file_type:str = 'csv'):
+        try:
+            if file_type == 'csv':
+                df = pd.read_csv(file_name)
+            elif file_type == 'tab':
+                df = pd.read_csv(file_name,sep=' ')
+            return df
+        except Exception as e:
+            raise Exception(e)
+
+class text_preprocessing():
+    def __init__(self,file_name:str = '',file_type:str = ''):
+        self.file_name = file_name
+        self.file_type = file_type
+    def text_cleaning(self,unwanted_cols:list = None):
+        try:
+            ingest = data_ingestion()
+            df = ingest.ingest_data(self.file_name,self.file_type)
+            
+            #Removing the unwanted columns that are not considered in the perview of the sentiment analysis
+            df = self.__remove_unwanted_cols(df,unwanted_cols)
+            
+            #Removing nan values
+            df = self.__remove_null_values(df)
+            
+            #Converting the text into lower case letters
+            df['reviewText'] = df['reviewText'].str.lower()
+            
+            #Removing special characters from the review text
+            df['reviewText'] = df['reviewText'].apply(lambda x : re.sub('[^a-zA-Z0-9]',' ',x))
+
+            #Removing extra white spaces
+            df['reviewText'] = df['reviewText'].apply(lambda x : re.sub(r"\s+",' ',x).strip())
+
+            #Handling negations:
+            df['reviewText'] = df['reviewText'].apply(lambda x : re.sub(r"\bnot\s+(\w+)",r"not_\1",x))
+
+            #Handling Very Short Words:
+            df['reviewText'] = df['reviewText'].apply(
+                lambda x : ' '.join([word for word in x.split() if len(word) > 2])
+            )
+            #Handling Emoji
+            df['reviewText'] = df['reviewText'].apply(lambda x : emoji.demojize(x))
+
+            #Handling urls and html
+            df['reviewText'] = df['reviewText'].apply(lambda x : re.sub(r"https\S+|www\S+",'',x))
+            df['reviewText'] = df['reviewText'].apply(lambda x : re.sub(r"<.*?>",'',x))
+            return df
+        except Exception as e:
+            raise Exception(e)
+    
+    def __remove_unwanted_cols(self,dframe:pd.DataFrame = None,unwanted_cols:list = None):
+        try:
+            if unwanted_cols is None:
+                return dframe
+            else :
+                return dframe.drop(columns=unwanted_cols)
+        except Exception as e:
+            raise Exception(e)
+        
+    def __remove_null_values(self,dframe:pd.DataFrame = None):
+        try:
+            for col in dframe.columns:
+                nan_occ = dframe[col].isna().mean()
+                if nan_occ >= 0.05:
+                    dframe[col] = dframe[col].fillna('',axis=0)
+                elif nan_occ < 0.05:
+                    dframe = dframe.dropna(subset=[col],axis=0)
+            return dframe
+        except Exception as e:
+            raise Exception(e)
+        
+    def __tokenizing_corpus(self,dframe:pd.DataFrame = None):
+        try:
+            documents = dframe['reviewText'].tolist()
+            stop_words = stopwords.words('english')
+            corpus = []
+            lemma = WordNetLemmatizer()
+            for i in tqdm(range(len(documents))):
+                documents[i] = word_tokenize(documents[i])
+                documents[i] = [lemma.lemmatize(word) for word in documents[i] if word not in stop_words]
+                documents[i] = ' '.join(documents[i])
+                corpus.append(documents[i])
+            return(corpus)
+        except Exception as e:
+            raise Exception(e)
+
+    def features_setting(self,dframe:pd.DataFrame = None):
+        try:
+            X = self.__tokenizing_corpus(dframe=dframe)
+            '''
+            Encoding for ratings
+            if rating >= 3 then 1 which means positive
+            if rating < 3 then 0 which means negative
+            ''' 
+            dframe['overall'] = dframe['overall'].apply(lambda x : 1 if x >= 3 else 0)
+            y = dframe['overall']
+            return X,y
+        except Exception as e:
+            raise Exception(e)
